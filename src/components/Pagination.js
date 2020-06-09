7a25b56d882160248304ce55de18b71c96b71ca8
Mahmudul Hasan Sreejon <mahmudul-hasan-sreejon@users.noreply.github.com>
2020-06-10 04:12:31 +0600

    Add pagination and render all data on startup

---
+++ src/components/Pagination.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+
+class Pagination extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentPage: props.currentPage,
+            totalPage: props.totalPage
+        };
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick(event) {
+        let currentPage = event.target.innerText;
+        axios
+        .get(`http://localhost:3001/shipments?_page=${currentPage}&_limit=${this.props.limit}`)
+        .then((response) => {
+            this.props.handleClick(response.data);
+            this.setState({currentPage: parseInt(currentPage)});
+        });
+    }
+
+    renderIndex(index) {
+        return (
+            <span key={index} className="index-box" onClick={this.handleClick}>{index}</span>
+        );
+    }
+
+    render() {
+        let pagination = [];
+        for (let index = 1; index <= this.props.totalPage; index++) {
+            pagination.push(this.renderIndex(index));
+        }
+        return (
+            <div className="pagination">
+                {pagination}
+            </div>
+        );
+    }
+}
+
+export default Pagination;
--- src/components/Shipments.js
+++ src/components/Shipments.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import ShipmentBlock from './ShipmentBlock';
 import Search from './Search';
 import Sorting from './Sorting';
+import Pagination from './Pagination';
 
 
 class Shipments extends Component {
@@ -10,8 +12,21 @@ class Shipments extends Component {
         super(props);
         this.updateShipmentDetail = this.updateShipmentDetail.bind(this);
         this.state = {
-            shipmentDetail: []
+            shipmentDetail: [],
+            currentPage: 1,
+            totalPage: 0,
+            limitPerPage: 20,
+            currentPageData: []
         };
+    }
+
+    componentDidMount() {
+        axios
+        .get(`http://localhost:3001/shipments?_page=${this.state.currentPage}&_limit=${this.state.limitPerPage}`)
+        .then((response) => {
+            let totalPage = parseInt(response.data.length / this.state.limitPerPage) + 1;
+            this.setState({shipmentDetail: response.data, totalPage});
+        });
     }
 
     updateShipmentDetail(data) {
@@ -33,6 +48,13 @@ class Shipments extends Component {
                 <div>
                     {this.state.shipmentDetail.map((data) => this.renderShipmentBlock(data))}
                 </div>
+
+                <Pagination
+                    totalPage= {this.state.totalPage}
+                    currentPage={this.state.currentPage}
+                    limit={this.state.limitPerPage}
+                    handleClick={this.updateShipmentDetail}
+                />
             </div>
         );
     }
